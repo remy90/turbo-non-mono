7d70984e9daf015b6d1ef3e0bafbb34f65f8b90c
Remy <jsgaisie@outlook.com>
2023-03-20 16:59:43 +0000

    outputFileTracingRoot

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,21 @@
+const path = require('path');
+console.log("envs", process.env.NEXT_PRIVATE_STANDALONE, process.env.AMPLIFY_MONOREPO_APP_ROOT)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // distDir: './.next',
-  // output: 'standalone',
-  basePath: "",
+  distDir: './.next',
+  output: 'standalone',
   cleanDistDir: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingIncludes: {
+      '/apps/web': ['./.next/'],
+    },
+  },
 };
 
 module.exports = nextConfig;
