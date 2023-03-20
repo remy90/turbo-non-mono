3e3c106d062d601320bf9e9e013cae2450d8d967
Remy <jsgaisie@outlook.com>
2023-03-20 10:31:41 +0000

    standalone

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: './.next',
+  output: 'standalone',
+  basePath: "",
+  cleanDistDir: true,
 };
 
 module.exports = nextConfig;
